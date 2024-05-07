4f41e331e03d9531fb745e3c8be90cd1402ea2c5
Joe Rrobin <joerobin26@gmail.com>
2024-05-07 10:42:11 +0530

    feat: test route

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,13 +1,9 @@
-import { getDb } from '@/lib/mongodb'
 import { NextRequest } from 'next/server'
 import { z } from 'zod'
 
 export async function POST(req: NextRequest) {
   try {
-    const db = await getDb()
-    const body = await req.json()
-    const res = await db.collection('exec').insertOne(body)
-    return Response.json(res)
+    return Response.json({ message: 'hi this is a test' })
   } catch (error) {
     return new Response('error', { status: 500 })
   }
